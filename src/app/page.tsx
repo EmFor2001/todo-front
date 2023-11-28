1fc89e60f7bd26bce4490c8ec5ef97deefefa5ee
EmFor2001 <k.hajduk.kh@gmail.com>
2023-11-28 15:02:27 +0100

    fetch todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const RootContainer = styled.div`
@@ -11,7 +12,7 @@ const RootContainer = styled.div`
 
 const TitleContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
@@ -30,7 +31,62 @@ const Subtitle = styled.p`
   padding: 0;
 `;
 
+const TodoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TodoTitle = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+`;
+
+const TodoList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TodoItem = styled.li`
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+`;
+
+const TodoItemTitle = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+`;
+
+const TodoItemDescription = styled.p`
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+`;
+
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+
+  async function getTodos() {
+    const response = await fetch("https://localhost:7085/ToDo");
+    const data = await response.json();
+    console.log(data);
+    setTodos(data);
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <>
       <RootContainer>
@@ -38,6 +94,17 @@ export default function Home() {
           <Title>Todo App</Title>
           <Subtitle>Full-stack todo</Subtitle>
         </TitleContainer>
+        <TodoContainer>
+          <TodoTitle>Todo List</TodoTitle>
+          <TodoList>
+            {todos.map((todo: any) => (
+              <TodoItem key={todo.id}>
+                <TodoItemTitle>{todo.title}</TodoItemTitle>
+                <TodoItemDescription>{todo.description}</TodoItemDescription>
+              </TodoItem>
+            ))}
+          </TodoList>
+        </TodoContainer>
       </RootContainer>
     </>
   );
